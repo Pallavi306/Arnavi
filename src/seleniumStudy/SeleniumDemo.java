87ddf4ee97989a5583562d96aaf302f4d634f790
Pallavi306 <pallavishirude57@gmail.com>
2022-08-18 00:54:49 +0530

    created files

---
+++ src/seleniumStudy/SeleniumDemo.java
@@ -0,0 +1,13 @@
+package seleniumStudy;
+
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumFolder\\chromedriver.exe");
+		 
+         		
+	}
+
+}
